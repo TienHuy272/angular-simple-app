b552296cee3d2e4d1cae1d27f0064be1777f50b7
HuyNT <tienhuylionel@gmail.com>
2024-10-29 08:58:59 +0700

    save data tasks to localstorage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -6,6 +6,13 @@ import { NewTask } from './task/task.model';
 export class TasksService {
     private tasks = dummyTasks;
 
+    constructor() {
+        const tasks = localStorage.getItem("tasks");
+        if (tasks) {
+            this.tasks = JSON.parse(tasks);
+        } 
+    }
+
     getUserTasks(userId: string) {
         return this.tasks.filter((task) => task.userId === userId);
     }
@@ -18,9 +25,15 @@ export class TasksService {
           summary: newTask.summary,
           dueDate: newTask.date
         })
+        this.saveTasks();
     }
 
     removeTask(id: string) {
         this.tasks = this.tasks.filter(task => task.id !== id)
+        this.saveTasks();
+    }
+
+    private saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks))
     }
 }
